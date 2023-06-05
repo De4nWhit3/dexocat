7ef10da08475147dcec0a84164b2868e34caca5a
Dean White <de4nwhit3@gmail.com>
2023-06-05 13:46:31 +0200

    added end drawer with placeholder

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,16 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      endDrawer: Drawer(
+        child: ListView(
+          children: const [
+            DrawerHeader(
+              child: Placeholder(),
+            ),
+            Placeholder(),
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
